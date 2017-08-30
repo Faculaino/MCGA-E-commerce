a3e9e120633c9e310ca3e206e4b8dc909f9e1d8b
Faculaino <faculaino@yahoo.com.ar>
2017-08-30 18:08:30 -0300

    Inicio

---
+++ SolutionsLeatherGoods/Presentation/ASF.UI.WbSite/Areas/Category/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASF.UI.WbSite.Areas.Category.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category/Category
+        public ActionResult Index()
+        {
+            var categorias = new UI.Process.CategoryProcess();
+            return View(categorias.SelectList());
+        }
+    }
+}
---
+++ SolutionsLeatherGoods/Presentation/ASF.UI.WbSite/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASF.UI.WbSite.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            var categorias = new UI.Process.CategoryProcess();
+            return View(categorias.SelectList());
+        }
+    }
+}
