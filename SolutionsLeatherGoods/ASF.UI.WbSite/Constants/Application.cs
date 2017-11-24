7ca0744901b08aa9649aec09f8e051ad28e59155
Faculaino <faculaino@yahoo.com.ar>
2017-11-24 03:08:39 -0300

    Template FrontEnd

---
+++ SolutionsLeatherGoods/ASF.UI.WbSite/Constants/Application.cs
@@ -0,0 +1,8 @@
+﻿namespace ASF.UI.WbSite.Constants
+{
+    public class Application
+    {
+        public const string Name = "E-Commerce";
+        public const string ShortName = "ASF";
+    }
+}
---
+++ SolutionsLeatherGoods/ASF.UI.WbSite/Constants/ControllerName.cs
@@ -0,0 +1,13 @@
+﻿namespace ASF.UI.WbSite.Constants
+{
+    public static class ControllerName
+    {
+        public const string Error = "Error";
+        public const string Home = "Home";
+        public const string Category = "Category";
+        public const string Client = "Client";
+        public const string Dealer = "Dealer";
+        public const string Country = "Country";
+        public const string Product = "Product";
+    }
+}
